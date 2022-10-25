3ab9cfc5c48fa07e85d026bb7ac2d377725c3ceb
Yonghwi Lee <101785899+yooong@users.noreply.github.com>
2022-10-25 19:20:55 +0900

    Chage line32

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def bokeh(condition=None, feature=None):
         fig.line('Datetime', i, source=source)
         fig.title.text='{}'.format(i.upper())
         fig.xaxis.axis_label='TimeStamp'
-        fig.yaxis.axis_label='Value'
+        fig.yaxis.axis_label='Values'
         fig_lst.append(fig)
 
     print(fig_lst)
